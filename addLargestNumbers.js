0615d62c7e34186850b5c80821c37911d3473407
YumiMachino <y.y.1515m@gmail.com>
2020-08-25 02:11:51 +0900

    answered on addLargestNumbers

--- addLargestNumbers.js
+++ addLargestNumbers.js
@@ -1,7 +1,13 @@
 const addLargestNumbers = function(data) {
   // Put your solution here
+
+  let sum = 0;
+  for (i = 0; i < data.length; i++) {
+    sum += data[i];
+  }
+  return sum;
 };
 
-console.log(addLargestNumbers([1, 10]));
-console.log(addLargestNumbers([1, 2, 3]));
+// console.log(addLargestNumbers([1, 10]));
+// console.log(addLargestNumbers([1, 2, 3]));
 console.log(addLargestNumbers([10, 4, 34, 6, 92, 2]));
