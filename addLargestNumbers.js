5db84893d22531edfb1f073d038e3f63898a25ca
YumiMachino <y.y.1515m@gmail.com>
2020-08-25 02:54:16 +0900

    update on addLargeNumbers.js

--- addLargestNumbers.js
+++ addLargestNumbers.js
@@ -1,8 +1,15 @@
 const addLargestNumbers = function(data) {
   // Put your solution here
+  //sum the 2 largest numbers
+
+  function compareFunc(a, b) {
+    return a - b;
+  }
+
+  data = data.sort(compareFunc);
 
   let sum = 0;
-  for (i = 0; i < data.length; i++) {
+  for (i = data.length - 2; i <= data.length - 1; i++) {
     sum += data[i];
   }
   return sum;
