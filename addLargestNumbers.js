f7a175d432f426a77ea2ad04529f5bd5b6100e7e
YumiMachino <y.y.1515m@gmail.com>
2020-08-25 03:15:43 +0900

    answered conditionalAdding.js

--- addLargestNumbers.js
+++ addLargestNumbers.js
@@ -2,12 +2,14 @@ const addLargestNumbers = function(data) {
   // Put your solution here
   //sum the 2 largest numbers
 
+  //comparison function from small to large
   function compareFunc(a, b) {
     return a - b;
   }
-
+  //sort the array in numerical order
   data = data.sort(compareFunc);
 
+  //add the last 2 contents of the array
   let sum = 0;
   for (i = data.length - 2; i <= data.length - 1; i++) {
     sum += data[i];
--- conditionalAdding.js
+++ conditionalAdding.js
@@ -2,9 +2,26 @@
 
 const conditionalAdding = function(values, condition) {
   // Your code here
+  let sum = 0;
+  for (i = 0; i < values.length; i++) {
+    if (condition === "even") {
+      if (values[i] % 2 === 0) {
+        sum += values[i];
+      } else {
+        sum = sum;
+      }
+    } else if (condition === "odd") {
+      if (values[i] % 2 !== 0) {
+        sum += values[i];
+      } else {
+        sum = sum;
+      }
+    }
+  }
+  return sum;
 };
 
-console.log(conditionalAdding([1, 2, 3, 4, 5], "even"));
-console.log(conditionalAdding([1, 2, 3, 4, 5], "odd"));
-console.log(conditionalAdding([13, 88, 12, 44, 99], "even"));
+// console.log(conditionalAdding([1, 2, 3, 4, 5], "even"));
+// console.log(conditionalAdding([1, 2, 3, 4, 5], "odd"));
+// console.log(conditionalAdding([13, 88, 12, 44, 99], "even"));
 console.log(conditionalAdding([], "odd"));
