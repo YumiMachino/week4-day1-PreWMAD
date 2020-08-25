b91ebdc36e7e08d1a817cf31c6a7cb5829aeb746
YumiMachino <y.y.1515m@gmail.com>
2020-08-25 23:30:22 +0900

    answered vowelCounter.js

--- vowelCounter.js
+++ vowelCounter.js
@@ -3,8 +3,28 @@
 const vowelCounter = function(data) {
   // Put your solution here
   //using counter??
+  let dataArray = data.split("");
+  // console.log(dataArray.length);
+  let vowelCounter = 0;
+
+  for (n = 0; n < dataArray.length; n++) {
+    if (dataArray[n] === "a") {
+      vowelCounter += 1;
+    } else if (dataArray[n] === "e") {
+      vowelCounter += 1;
+    } else if (dataArray[n] === "i") {
+      vowelCounter += 1;
+    } else if (dataArray[n] === "o") {
+      vowelCounter += 1;
+    } else if (dataArray[n] === "u") {
+      vowelCounter += 1;
+    } else {
+      vowelCounter = vowelCounter;
+    }
+  }
+  return vowelCounter;
 };
 
-console.log(vowelCounter("orange"));
-console.log(vowelCounter("Cornerstone"));
+// console.log(vowelCounter("orange"));
+// console.log(vowelCounter("Cornerstone"));
 console.log(vowelCounter("aeiou"));
