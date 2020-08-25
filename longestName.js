0d5040cdb04f75e71f840df9d005d24600364792
YumiMachino <y.y.1515m@gmail.com>
2020-08-25 23:16:04 +0900

    update answer on longestName.js

--- longestName.js
+++ longestName.js
@@ -7,35 +7,39 @@ const LongestName = function(instructors) {
   let fullName;
   let fullNameCount = 0;
   let theLongest = 0;
+  let theLongestName = ""; //as a string
 
   for (i = 0; i < instructors.length; i++) {
     fullName = instructors[i].first + instructors[i].last; //combine first and last name
     fullNameCount = fullName.length; //count number of the string
-    if (theLongest > fullNameCount) {
-      theLongest = fullNameCount;
-    } else if (theLongest < fullNameCount) {
+
+    if (fullNameCount > theLongest) {
+      fullNameCount = theLongest;
+      theLongestName = instructors[i];
+    } else {
       theLongest = theLongest;
+      theLongestName = theLongestName;
     }
-    return theLongest;
   }
+  return theLongestName;
 };
 
-console.log(
-  LongestName([
-    //instructor[0]
-    { first: "Samuel", last: "Sanderson" },
-    //instructor[1]
-    { first: "Jeremiah", last: "Web" },
-    //instructor[2]
-    { first: "Ophilia", last: "Rich" },
-    //instructor[3]
-    { first: "Donald", last: "kant" }
-  ])
-);
 // console.log(
 //   LongestName([
-//     { first: "Matthew", last: "Ebert" },
-//     { first: "David", last: "John" },
-//     { first: "Domascus", last: "Anderson" }
+//     //instructor[0]
+//     { first: "Samuel", last: "Sanderson" },
+//     //instructor[1]
+//     { first: "Jeremiah", last: "Web" },
+//     //instructor[2]
+//     { first: "Ophilia", last: "Rich" },
+//     //instructor[3]
+//     { first: "Donald", last: "kant" }
 //   ])
 // );
+console.log(
+  LongestName([
+    { first: "Matthew", last: "Ebert" },
+    { first: "David", last: "John" },
+    { first: "Domascus", last: "Anderson" }
+  ])
+);
